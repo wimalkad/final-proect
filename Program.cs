7c6dace1d56b5c0de2ec0a2157865d8ce71d777e
Danil Zharikov <danil.zharikov@list.ru>
2023-01-20 16:35:03 +0300

    added program code

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿string[] Ar1 = new string[6] {"pirat", ":)", "lol", "World", ")9()()","massiv"};
+string[] Ar2 = new string[Ar1.Length];
+
+    void SecondArray(string[] Ar1, string[] Ar2)
+    {
+        int count = 0;
+        for (int i = 0; i < Ar1.Length; i++)
+        {
+        if(Ar1[i].Length <= 3)  
+            {
+            Ar2[count] = Ar1[i];
+            count++;
+            }
+        }
+    }
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArray(Ar1, Ar2);
+PrintArray(Ar2);
